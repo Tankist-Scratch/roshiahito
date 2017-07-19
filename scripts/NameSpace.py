62be3da668ce9e12e619153db08cf0efe478a752
Andrew <tankist.scratch.p@gmail.com>
2017-07-19 11:19:56 +0300

    Create NameSpace.py

---
+++ scripts/NameSpace.py
@@ -0,0 +1,12 @@
+class NameSpace:
+    def __init__(self):
+        pass
+
+    def __setattr__(self, name, value):
+        self.__dict__[name] = value
+
+    def __getattr__(self, name):
+        return self.__dict__[name]
+
+    def add(self, name):
+        self.__dict__[name] = NameSpace()
