c8481dc736957a2a6d4d61949585737d19241799
tankist-scratch <tankist.scratch.p@gmail.com>
2017-10-18 23:14:58 +0300

    VD-U => RW; vertical-align:top in menu

--- data.py
+++ data.py
@@ -5,14 +5,17 @@ header = """<img src='/roshiahito/logo.png' width='200px'><br>
  • Short link <a href='https://goo.gl/g5SiYW'>goo.gl/g5SiYW</a>"""
 
 struct = ["<!DOCTYPE html><html lang='ru'><head>",
-          "\n</head><body bgcolor='#dfe'>\n<table width='100%' bgcolor='#dfe'><tr><td width='200px' rowspan='2' style='background-color:#fff'>\n",
-          "\n<td rowspan='2'></td></td><td abgcolor='#fff'><h1>\n", "\n<br></h1></td></tr>\n<tr><td bgcolor='#fff'>",
-          "</td></tr></table>\n</body></html>"]
+          "\n</head><body bgcolor='#dfe'>\n<table width='100%' bgcolor='#dfe'><tr><td width='200px' rowspan='2' style='background-color:#fff; vertical-align:top;'>\n",
+          "\n<td rowspan='3'></td></td><td abgcolor='#fff'><h1>\n", "\n<br></h1></td></tr>\n<tr><td rowspan='2' bgcolor='#fff'>",
+          "</td></tr><tr><td></td></tr></table>\n</body></html>"]
 
 main = """"""
 
 pages = {
-    "VD-U": """{{Card|VD-U||L||T.S|40|40|10|2}}VD-U - первый робот ветки внедорожников. Первый раз принял участие в [[Robofinist 2017|'Ралли по коридору']] в 2017 году.<br>""""",
+    "RW-U": """{{Card|RW-U||L||T.S|40|40|10|2}}RW-U - первый робот ветки внедорожников. Первый раз принял участие в [[Robofinist 2017|'Ралли по коридору']] в 2017 году.<br>
+    
+    <h2>История</h2>
+    """"",
     "Robofinist 2017": """{{BCard|Robofinist|28/10/2017 – 29/10/2017|https://robofinist.ru/event/info/competitions/id/132#kind957}}""",
     "T.Tr": """{{Card|T.Tr||mB|||20|19.5|10|1}}T.Tr - самый первый робот собраный на MakeBlock. Хорошо подходит для езды по линии.<br>
          
@@ -21,11 +24,11 @@ pages = {
          
          <h2>Соревнования</h2>
          {{battle|Собери своего робота 2017|Полигон|Собери своего робота 2017|1|T.Tr Line-U}}""",
-    "T.S": """{{Card|T.S||L|VD-U||40|40|10|2}}T.S - тележка построеная на базе [[VD-U|VD-U]]. Унаследовала помещение электроники в трубу, что увеличивает количество мест для крепления.""",
+    "T.S": """{{Card|T.S||L|RW-U||40|40|10|2}}T.S - тележка построеная на базе [[RW-U|RW-U]]. Унаследовала помещение электроники в трубу, что увеличивает количество мест для крепления.""",
     "Собери своего робота 2017": """{{BCard|Собери своего робота|08/10/2017|http://belsmus.ru/tenders/Konkurs-%22Soberi-svoego-robota-2017%22}}
     
     <h2>Участники</h2>
     {{battleS|T.Tr Line-U|T.Tr|1|Полигон}}"""}
-all = ["VD"]
+all = ["RW"]
 
 styles = {""""""}
